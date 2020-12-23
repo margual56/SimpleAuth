42c75cf1b08cbeae49ea3fa713504517bd7071fc
Marcos Gutiérrez Alonso <margual56@gmail.com>
2020-12-23 23:01:55 +0100

    Crappy test

--- main.py
+++ main.py
@@ -4,65 +4,61 @@ from Database import DatabaseWrapper
 import os
 import sys
 
-app = Flask(__name__, static_folder="static")
-db = None
 
+def main():
+    app = Flask(__name__, static_folder="static")
+    db = None
 
-@app.route('/')
-def home():
-    if session.get('logged_in'):
-        return render_template('controlpanel.html')
-    else:
-        return redirect("/login")
-
-
-@app.route('/login', methods=['POST', 'GET'])
-def login(incorrect=False):
-    if request.method == 'POST':
-        correct = db.checkUser(request.form['username'], request.form['password'])
-
-        if correct:
-            session['logged_in'] = True
-            return redirect(url_for('home'))
-
-        return render_template('login.html', incorrect=True)
-    else:
+    @app.route('/')
+    def home():
         if session.get('logged_in'):
-            return redirect(url_for('home'))
+            return render_template('controlpanel.html')
         else:
-            return render_template('login.html', incorrect=False)
+            return redirect("/login")
 
+    @app.route('/login', methods=['POST', 'GET'])
+    def login(incorrect=False):
+        if request.method == 'POST':
+            correct = db.checkUser(request.form['username'], request.form['password'])
 
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return home()
+            if correct:
+                session['logged_in'] = True
+                return redirect(url_for('home'))
 
+            return render_template('login.html', incorrect=True)
+        else:
+            if session.get('logged_in'):
+                return redirect(url_for('home'))
+            else:
+                return render_template('login.html', incorrect=False)
 
-@app.route("/newaccount", methods=['POST', 'GET'])
-def newuser():
-    if request.method == "POST":
-        if request.form['password1'] == request.form['password2']:
-            if db.userExists(request.form['username']):
-                flash("User exists")
+    @app.route("/logout")
+    def logout():
+        session['logged_in'] = False
+        return home()
+
+    @app.route("/newaccount", methods=['POST', 'GET'])
+    def newuser():
+        if request.method == "POST":
+            if request.form['password1'] == request.form['password2']:
+                if db.userExists(request.form['username']):
+                    flash("User exists")
+                    return redirect(url_for('login'))
+
+                db.newUser(request.form['username'], request.form['password1'])
+
                 return redirect(url_for('login'))
-
-            db.newUser(request.form['username'], request.form['password1'])
-
-            return redirect(url_for('login'))
+            else:
+                flash("User exists")
+                return redirect(url_for('newuser'))
         else:
-            flash("User exists")
-            return redirect(url_for('newuser'))
-    else:
-        return render_template('newuser.html')
+            return render_template('newuser.html')
+
+    # @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
 
 
-# @app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
-
-
-if __name__ == '__main__':
     production = False
 
     if len(sys.argv) == 2:
